1e2069992bccf278833598c0bfec07b7e813b51d
Joshua <joshsmayhew@gmail.com>
2023-03-07 14:15:03 +0700

    commit tree bfdb3eec9f514f1d2e128b72d16324c2bd6dd0f8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def show
+    user = find_user
+    render json: user, include: :items, status: :ok
+  end
+
+  private
+
+  def find_user
+    User.find(params[:id])
+  end
 end
